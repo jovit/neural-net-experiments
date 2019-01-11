f1c4cafc2cdfde51d81bf93c049cb366c15074b9
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2019-01-11 14:20:54 -0200

    more python

--- python/network/Brain.py
+++ python/network/Brain.py
@@ -61,66 +61,46 @@ class Brain:
     else:
         self.current_batch += 1
 
-  def calculate_gradient_neuron(neuron, current_layer, expected_output) {
+  def calculate_gradient_neuron(self, neuron, current_layer, expected_output):
     neuron_obj = self.layers[currentLayer][neuron]
 
-    if current_layer ==  self.layers.length - 1:
-      // last layer
-      neuron_obj.relation_to_cost =
-        2 * (neuron_obj.get_activation_sigmoid() - expected_output)
+    if current_layer == self.layers.length - 1:
+      neuron_obj.relation_to_cost = 2 * \
+          (neuron_obj.get_activation_sigmoid() - expected_output)
     else:
-      neuron_obj.relation_to_cost = self.layers[currentLayer + 1]
-        .map(
-          it=>
-            it.relationToCost *
-            it.synapses[neuron].weight *
-            it.getActivationSigmoid() *
-            (1 - it.getActivationSigmoid())
-        )
-        .reduce((a, b) = > a + b);}
+      neuron_obj.relation_to_cost = reduce(lambda a, b: a + b, map(lambda it: it.relationToCost *
+                                           it.synapses[neuron].weight * it.get_activation_sigmoid() * (1 - it.get_activation_sigmoid()), self.layers[currentLayer + 1]))
 
-    neuron_obj.biasGradient +=
-      neuron_obj.getActivationSigmoid() *
-      (1 - neuron_obj.getActivationSigmoid()) *
-      neuron_obj.relationToCost;
+    neuron_obj.bias_gradient = neuron_obj.bias_gradient + neuron_obj.get_activation_sigmoid() * \
+                                                                  (1 - neuron_obj.get_activation_sigmoid()
+                                                                   ) * neuron_obj.relation_to_cost
 
-    for (let synapse in neuron_obj.synapses) {
-      const gradient =
-        neuron_obj.synapses[synapse].parentNeuron.getActivationSigmoid() *
-        neuron_obj.getActivationSigmoid() *
-        (1 - neuron_obj.getActivationSigmoid()) *
-        neuron_obj.relationToCost;
+    for synapse in neuron_obj.synapses:
+        gradient = synapse.parent_neuron.get_activation_sigmoid() *
+          neuron_obj.get_activation_sigmoid() *
+          (1 - neuron_obj.get_activation_sigmoid()) *
+          neuron_obj.relation_to_cost
 
-      neuron_obj.synapses[synapse].gradient += gradient; }
+        synapse.gradient += gradient
 
     return neuron_obj
 
-  applyBackwardsPropagation(expectedOutput, layer=self.layers.length - 1) {
-    if (layer == = 0) {
-      return; }
-    for (let i=0; i < self.layers[layer].length; i++) {
-      self.layers[layer][i] = self.calculateGradientNeuron(
+  def apply_backwards_propagation(self, expected_output, layer=self.layers.length - 1):
+    if layer == 0:
+      return
+    for i in range(self.layers[layer].length):
+      self.layers[layer][i] = self.calculate_gradient_neuron(
         i,
         layer,
         expectedOutput[i]
       ); }
-    self.applyBackwardsPropagation(expectedOutput, layer - 1);}
+    self.apply_backwards_propagation(expected_output, layer - 1)
 
-  applyGradients(batchSize) {
-    for (let layer in self.layers) {
-      for (let neuron in self.layers[layer]) {
-        self.layers[layer][neuron].biasGradient =
-          self.layers[layer][neuron].biasGradient / batchSize;
-        self.layers[layer][neuron].applyGradient();
-        for (let synapse in self.layers[layer][neuron].synapses) {
-          self.layers[layer][neuron].synapses[synapse].gradient =
-            self.layers[layer][neuron].synapses[synapse].gradient / batchSize;
-          self.layers[layer][neuron].synapses[synapse].applyGradient(); }
-      }
-    }
-  }
-
-
-}
-
-module.exports = Brain;
+  def apply_gradients(self, batchSize):
+    for layer in self.layers:
+      for neuron in layer:
+        neuron.biasGradient = neuron.biasGradient / batchSize
+        neuron.applyGradient()
+        for synapse in neuron.synapses:
+          synapse.gradient = synapse.gradient / batchSize
+          synapse.applyGradient()
